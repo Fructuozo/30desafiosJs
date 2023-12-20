2a4e20b0e85a46810c7cb66b8b05393b913a666b
Gabriel Fructuozo <gabrielfruc@gmail.com>
2023-12-20 20:46:31 -0300

    teste

---
+++ muitoFácil/01desafio.js
@@ -0,0 +1,17 @@
+function average(numbers) {
+    let sum = 0
+
+    numbers.forEach(number => {
+        sum += number //sum = sum + number
+    });
+
+    const average = sum / numbers.length
+
+    return average
+};
+
+
+console.log(average([10, 9, 6, 8, 9, 1, 5, 7]));
+console.log(average([2, 5, 7, 1, -2]));
+console.log(average([10, 10, 10, 10, 9]));
+console.log(average([25, 75]));
